3408d7f1c73ac2d58c35fff4a585074fa34555c2
François Gaudin <francois@gaudin.email>
2026-08-15 07:52:17 +0200

    improves look of search text box

--- src/App.tsx
+++ src/App.tsx
@@ -210,6 +210,7 @@ const App: React.FC = () => {
 
                 {/* Track search */}
                 <input
+                    className="search-input"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     placeholder="Search for treasure songs..."
